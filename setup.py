a86a24590d7701bc8310e79fb04c6ebba0f19af7
Kirill Klyukvin <kir.klyukvin@gmail.com>
2021-05-22 20:35:04 +0300

    0.1.2

--- setup.py
+++ setup.py
@@ -1,10 +1,5 @@
 from setuptools import setup
 import sys
-import os 
-
-#this_directory = path.abspath(path.dirname(__file__))
-#with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-#    long_description = f.read()
 
 if sys.version_info[0] < 3:
     with open('README.md') as f:
@@ -13,11 +8,14 @@ else:
     with open('README.md', encoding='utf-8') as f:
         long_description = f.read()
 
+project_urls = {'Homepage' : 'https://github.com/cptcroissant/logregnumpy'}
+
 setup(name='logregnumpy',
       version='0.1.2',
       description='Logistic Regression Classifier',
       long_description=long_description,
       long_description_content_type='text/markdown',
       packages=['logregnumpy'],
+      project_urls=project_urls,
       author_email='kir.klyukvin@gmail.com',
       zip_safe=False)
